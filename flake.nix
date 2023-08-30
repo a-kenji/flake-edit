08d3922e85e97e35cc519256eb0f1cabd3c6b55f
a-kenji <aks.kenji@protonmail.com>
2023-08-30 20:52:46 +0200

    chore(fmt): add nightly formatter

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
       rustFmtToolchainTOML = rustPkgs.rust-bin.fromRustupToolchainFile RUSTFMT_TOOLCHAIN;
 
       rustToolchainDevTOML = rustToolchainTOML.override {
-        extensions = ["rustfmt" "clippy" "rust-analysis" "rust-docs"];
+        extensions = ["clippy" "rust-analysis" "rust-docs"];
         targets = [];
       };
       gitDate = "${builtins.substring 0 4 self.lastModifiedDate}-${builtins.substring 4 2 self.lastModifiedDate}-${builtins.substring 6 2 self.lastModifiedDate}";
