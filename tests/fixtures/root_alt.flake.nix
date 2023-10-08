920917e0dccf78526421f9830284a868c08d348a
a-kenji <aks.kenji@protonmail.com>
2023-10-08 14:18:34 +0200

    add alternative

---
+++ tests/fixtures/root_alt.flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Manage your flake inputs comfortably.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-utelinos.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-overlay.follows = "rust-overlay";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = _: { };
+}
