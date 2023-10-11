690aa323e2b1159b5cf4c93c4e3db79faed0527c
a-kenji <aks.kenji@protonmail.com>
2023-10-11 17:31:04 +0200

    add

--- tests/fixtures/completely_flat_toplevel.flake.nix
+++ tests/fixtures/completely_flat_toplevel.flake.nix
@@ -29,27 +29,23 @@
   #   };
   # };
   #
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      flake-utelinos,
-      rust-overlay,
-      crane,
-    }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    flake-utelinos,
+    rust-overlay,
+    crane,
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
         stdenv =
-          if pkgs.stdenv.isLinux then
-            pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv
-          else
-            pkgs.stdenv
-        ;
-        overlays = [ (import rust-overlay) ];
-        rustPkgs = import nixpkgs { inherit system overlays; };
+          if pkgs.stdenv.isLinux
+          then pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv
+          else pkgs.stdenv;
+        overlays = [(import rust-overlay)];
+        rustPkgs = import nixpkgs {inherit system overlays;};
         src = self;
         RUST_TOOLCHAIN = src + "/rust-toolchain.toml";
         RUSTFMT_TOOLCHAIN = src + "/.rustfmt-toolchain.toml";
@@ -61,7 +57,7 @@
 
         rustFmtToolchainTOML =
           rustPkgs.rust-bin.fromRustupToolchainFile
-            RUSTFMT_TOOLCHAIN;
+          RUSTFMT_TOOLCHAIN;
 
         rustToolchainDevTOML = rustToolchainTOML.override {
           extensions = [
@@ -69,11 +65,11 @@
             "rust-analysis"
             "rust-docs"
           ];
-          targets = [ ];
+          targets = [];
         };
         gitDate = "${builtins.substring 0 4 self.lastModifiedDate}-${
-            builtins.substring 4 2 self.lastModifiedDate
-          }-${builtins.substring 6 2 self.lastModifiedDate}";
+          builtins.substring 4 2 self.lastModifiedDate
+        }-${builtins.substring 6 2 self.lastModifiedDate}";
         gitRev = self.shortRev or self.dirtyShortRev;
         cargoLock = {
           lockFile = builtins.path {
@@ -85,7 +81,7 @@
         rustc = rustToolchainTOML;
         cargo = rustToolchainTOML;
 
-        buildInputs = [ pkgs.installShellFiles ];
+        buildInputs = [pkgs.installShellFiles];
 
         devInputs = [
           rustToolchainDevTOML
@@ -151,10 +147,9 @@
           pkgs.taplo
           pkgs.typos
         ];
-        editorConfigInputs =
-          [
-            # pkgs.editorconfig-checker
-          ];
+        editorConfigInputs = [
+          # pkgs.editorconfig-checker
+        ];
         actionlintInputs = [
           pkgs.actionlint
           pkgs.shellcheck
@@ -187,13 +182,12 @@
         meta = with pkgs.lib; {
           homepage = "https://github.com/a-kenji/fe";
           description = "Edit your flake inputs with ease";
-          license = [ licenses.mit ];
+          license = [licenses.mit];
         };
-      in
-      rec {
+      in rec {
         devShells = {
           default = devShells.fullShell;
-          fullShell = (pkgs.mkShell.override { inherit stdenv; }) {
+          fullShell = (pkgs.mkShell.override {inherit stdenv;}) {
             buildInputs = fmtInputs ++ devInputs;
             inherit name;
             ASSET_DIR = assetDir;
@@ -202,13 +196,13 @@
             # RUSTFLAGS = "-C linker=clang -C link-arg=-fuse-ld=${pkgs.mold}/bin/mold -C target-cpu=native";
             RUSTFLAGS = "-C linker=clang -C link-arg=-fuse-ld=${pkgs.mold}/bin/mold";
           };
-          editorConfigShell = pkgs.mkShell { buildInputs = editorConfigInputs; };
-          actionlintShell = pkgs.mkShell { buildInputs = actionlintInputs; };
-          fmtShell = pkgs.mkShell { buildInputs = fmtInputs; };
+          editorConfigShell = pkgs.mkShell {buildInputs = editorConfigInputs;};
+          actionlintShell = pkgs.mkShell {buildInputs = actionlintInputs;};
+          fmtShell = pkgs.mkShell {buildInputs = fmtInputs;};
         };
         packages = {
           default = packages.crane;
-          upstream = (pkgs.makeRustPlatform { inherit cargo rustc; }).buildRustPackage {
+          upstream = (pkgs.makeRustPlatform {inherit cargo rustc;}).buildRustPackage {
             cargoDepsName = name;
             GIT_DATE = gitDate;
             GIT_REV = gitRev;
@@ -224,7 +218,7 @@
               postInstall
               src
               stdenv
-            ;
+              ;
           };
           crane = craneLib.buildPackage (
             commonArgs
@@ -251,9 +245,9 @@
                 cargoArtifacts
                 meta
                 # name
-
+                
                 stdenv
-              ;
+                ;
             }
           );
         };
