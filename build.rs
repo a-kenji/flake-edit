47416d6271003dfae45967dcf0b7804c063ae6a5
a-kenji <aks.kenji@protonmail.com>
2024-07-26 15:26:15 +0200

    chore: fix typo

--- build.rs
+++ build.rs
@@ -33,7 +33,7 @@ fn main() {
             generate_to(*shell, cmd, NAME.to_string(), out).unwrap();
             // claps completions generation mechanisms are very immature,
             // include self adjusted ones
-            // Explicily ignore patterns
+            // Explicitly ignore patterns
             #[allow(clippy::wildcard_in_or_patterns)]
             match shell {
                 Shell::Fish => {
